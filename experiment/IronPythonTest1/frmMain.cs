c729a7b6d793b093e915f080cd27b1de868a377e
Roy Wang <roy.wang@systemcom.com.tw>
2018-10-02 01:20:00 +0800

    iron-python test

---
+++ experiment/IronPythonTest1/frmMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//DLR => 配合 scripting language
+using System.Dynamic;
+using IronPython;
+using IronPython.Hosting;
+
+namespace IronPythonTest1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+
+            //1.先裝IronPython
+            //2.add Reference to IronPython.dll / IronPython.Modules.dll / Microsoft.Scripting.dll
+            //3.加入 using 
+            //enjoy it!
+            var engine = IronPython.Hosting.Python.CreateEngine();
+
+            string script = 
+@"
+print ""Hello World""
+raw_input()
+";
+            engine.Execute(script);
+        }
+    }
+}
