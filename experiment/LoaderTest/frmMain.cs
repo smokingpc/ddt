3864ecf2ec15d6fd267ef83d8b3405e5bd415387
Roy Wang <roy.wang@systemcom.com.tw>
2018-11-10 10:55:44 +0800

    增加 python import lib 並實驗runtime assign python home

---
+++ experiment/LoaderTest/frmMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoaderTest
+{
+    public partial class frmMain : Form
+    {
+        public static string ExePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        public static string PyLibPath = ExePath + @"\Libs";
+        public static string PyDLLPath = ExePath + @"\DLLs";
+        public static string PyHomePath = ExePath ;
+
+        //1.Distribution要把 C:\Python27\Lib , C:\Python27\Libs , C:\Python27\DLLs
+        //  複製到想要指定的python home 路徑下
+        //2.用Loader去判斷有沒有PythonHome變數，沒有的話指定到exe path
+        //3.launch it!
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.ProcessStartInfo startinfo = new System.Diagnostics.ProcessStartInfo();
+            startinfo.EnvironmentVariables["PYTHONHOME"] = ExePath;
+            string home = startinfo.EnvironmentVariables["PYTHONHOME"];
+
+            startinfo.UseShellExecute = false;
+            startinfo.WorkingDirectory = ExePath;
+            startinfo.FileName = "PythonNetTest1.exe";
+            System.Diagnostics.Process.Start(startinfo);
+        }
+    }
+}
--- experiment/PythonNetTest1/frmMain.cs
+++ experiment/PythonNetTest1/frmMain.cs
@@ -14,9 +14,18 @@ namespace PythonNetTest1
 {
     public partial class frmMain : Form
     {
+        public static string ExePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
         public frmMain()
         {
             InitializeComponent();
+
+            //必須在系統環境變數設定 PYTHONHOME ，指向python runtime目錄
+            //不然後面沒法執行python code，如果不想裝一大包python，可以把
+            // <%PYTHON_HOME%>\lib , <%PYTHON_HOME%>\libs , <%PYTHON_HOME%>\dlls 
+            //三個目錄一起擺到exe path下，在process一開始時指定好環境變數，
+            //Python Runtime Engine就會改變 PythonEngine.PythonHome 的指向位置
+            Environment.SetEnvironmentVariable("PYTHONHOME", ExePath);
         }
 
         //官方 python for C# 套件要用pip安裝，再去 %PythonHome%\Lib\site-packages\ 目錄下
@@ -25,11 +34,12 @@ namespace PythonNetTest1
         //     C#主程式也要把 prefer 32bit選項拿掉，不然跑不動
         private void button1_Click(object sender, EventArgs e)
         {
-            //必須在系統環境變數設定 PYTHONHOME ，指向python runtime目錄
-            //不然後面沒法執行python code
+            
+            //指定以後再init就會變更預設lib路徑，看來是Lazy Init
             string home = PythonEngine.PythonHome;
             string path = PythonEngine.PythonPath;
-
+            
+            //MessageBox.Show(home);
             //Python執行前要啟動python engine...
             PythonEngine.Initialize();
             //PythonNet不是thread safe，要multithread就得用這個鎖住整個引擎
